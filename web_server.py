a46e90ba66edaa31e7801915679e968718bad188
Kfir <kfir.carmely2@gmail.com>
2025-03-15 00:36:38 +0200

    Still Working on this stinky ahh post

--- web_server.py
+++ web_server.py
@@ -7,7 +7,6 @@ CODE_NOT_FOUND = "404 NOT FOUND"
 CODE_OK = "200 OK" # If the request was processed
 CODE_INTERNAL_SERVER = "500 INTERNAL SERVER ERROR" # If the response is empty
 CODE_CREATED = "201 CREATED" # If the file that was posted was created
-CODE_BAD_REQUEST = "400 BAD REQUEST" # problematic file sent with POST
 
 
 MIME_TYPES = {  "html": "text/html",
@@ -58,9 +57,8 @@ class Request: # A class handling the HTTP requests
             self.path = self.get_path()
 
 
-
     def check_request_type(self):
-        if self.data[:3] == "GET" or len(self.data):
+        if self.data[:3] == "GET" or len(self.data) == 3:
             return "GET"
         return "POST"
 
@@ -71,32 +69,34 @@ class Request: # A class handling the HTTP requests
             return ""
         return data.decode("utf-8")
 
-    def process_request(self): # Processes the data of the response as whole
-        if self.request_type == "GET":
-            while not self.data.endswith("\r\n\r\n"):
-                chunk = self.server.recv(1)
-                if not chunk:  # Handle disconnection
-                    self.path = ""
-                    self.data = ""
+    def process_request(self):
+        while not self.data.endswith("\r\n\r\n"):  # Read headers first
+            chunk = self.server.recv(1)
+            if not chunk:
+                self.path = ""
+                self.data = ""
+                break
+            else:
+                self.data += chunk.decode("utf-8")
+
+        if self.request_type == "POST":
+            # Extract Content-Length
+            headers = self.data.split("\r\n")
+            content_length = 0
+            for header in headers:
+                if header.lower().startswith("content-length:"):
+                    content_length = int(header.split(":")[1].strip())
                     break
-                else:
-                    self.data += chunk.decode("utf-8")
 
-        else:
-            counter = 0
-            while counter < 2:
-                chunk = self.server.recv(1)
-                if self.data.endswith("\r\n\r\n"):
-                    counter += 1
-
-                if not chunk:  # Handle disconnection
-                    self.path = ""
-                    self.data = ""
+            # Read the exact number of bytes specified in Content-Length
+            body = b""
+            while len(body) < content_length:
+                chunk = self.server.recv(min(1024, content_length - len(body)))
+                if not chunk:
                     break
-                else:
-                    self.data += chunk.decode("utf-8")
+                body += chunk
 
-
+            self.data = self.data.encode("utf-8") + body  # Append the body to request data
 
     def get_path(self): # Returns the path of the requested file
         path = self.data.split(' ', 2)[1].replace("+", " ")
@@ -106,47 +106,55 @@ class Request: # A class handling the HTTP requests
         return path
 
 class Response: # The class that handles with the HTTP responses
-    def __init__(self, path, request_type):
+    def __init__(self, path, request_type, data): # Constructor for get
         self.code = "" # The status code of the response
         self.file_type = "" # The type of the file
         self.path = path # The path to the file
 
         self.request_type = request_type
 
-        self.body = bytes()
-        self.get_body() # The body of the response (information that's being transferred)
+        self.data = data # For POST, the data that needs to be downloaded
 
+        self.body = bytes()
+        self.set_body() # The body of the response (information that's being transferred)
 
         self.headers = self.create_headers() # The headers of the response
 
-        self.msg = self.headers.encode("utf-8") + self.body
+        self.msg = self.headers.encode("utf-8") + self.body # The final text that will be sent to the client
 
     def check_file(self): # Checks if the path is right and sets the parameters
-        if self.path == "":
+        if self.path == "": # Checks if the response was empty
             self.code = CODE_INTERNAL_SERVER
             self.file_type = MIME_TYPES["txt"]
             self.body = ("Connection: close\r\n\r\n" + CODE_INTERNAL_SERVER).encode()
             return False
 
-        elif not os.path.isfile(self.path):
-            if "calculate_next" in self.path or "calculate-next" in self.path:
+        if self.request_type == "POST":
+            self.body = CODE_CREATED
+            self.code = CODE_OK
+            self.file_type = MIME_TYPES["txt"]
+            return True
+
+        elif not os.path.isfile(self.path) and self.request_type == "GET":
+            if "calculate_next" in self.path or "calculate-next" in self.path: # Checks if the path presented was a function
                 self.body = calculate_next(self.path).encode()
                 self.code = CODE_OK
                 self.file_type = MIME_TYPES["txt"]
                 return False
 
-            if "calculate_area" in self.path or "calculate-area" in self.path:
+            if "calculate_area" in self.path or "calculate-area" in self.path: # Checks if the path presented was a function
                 self.body = calculate_area(self.path).encode()
                 self.code = CODE_OK
                 self.file_type = MIME_TYPES["txt"]
                 return False
 
-            else:
+            else: # Returns that the file was not
                 self.code = CODE_NOT_FOUND
                 self.file_type = MIME_TYPES["txt"]
                 self.body = ("Connection: close\r\n\r\n" + CODE_NOT_FOUND).encode()
                 return False
 
+        # If the code is okay
         self.code = CODE_OK
         self.file_type = self.get_type()
         return True
@@ -165,13 +173,34 @@ class Response: # The class that handles with the HTTP responses
         except KeyError:
             return ""
 
+    def set_body(self): # Sets the body of the file
+        if self.request_type == "POST":
+            if self.check_file():
+                self.save_image()
 
-    def get_body(self): # Sets the body of the file
-        if self.check_file():
-            file = open(self.path, 'rb')
 
-            self.body = file.read()
-            file.close()
+        if self.request_type == "GET":
+            if self.check_file():
+                file = open(self.path, 'rb')
+
+                self.body = file.read()
+                file.close()
+
+    def save_image(self): # Takes the data from the POST request, and creates the file
+        boundary = self.data.split(b"Content-Type: ")[0].split(b"\r\n")[-1]  # Extract boundary
+        parts = self.data.split(boundary)[1:-1]  # Extract only file part
+        for part in parts:
+            if b"filename=" in part:
+                # Extract filename
+                name = part.split(b"filename=\"")[1].split(b"\"")[0].decode()
+                file_data = part.split(b"\r\n\r\n", 1)[1].rsplit(b"\r\n", 1)[0]  # Extract binary content
+
+                f = open(name, "wb")
+                f.write(file_data)
+                f.close()
+                return
+
+
 
 def calculate_next(file_path): # Returns the following number of the parameter that was passed, 4.5/6
     num = int(file_path.split("num=")[-1])
@@ -192,6 +221,7 @@ def calculate_area(file_path): # Returns the area of the triangle. 4.9
 
     return data
 
+
 def main(): # The main block of code
     os.chdir("webroot")
     server = Server()
@@ -201,10 +231,20 @@ def main(): # The main block of code
         server.start_client()
         request = Request(server)
 
-        print(request.data)
+        # print(request.data)
 
-        response = Response(request.path, request.request_type) # Creating the response
+
+
+        if not request.request_type == "GET":
+            response = Response(request.path, request.request_type, request.data) # Creating the response
+
+        else:
+            response = Response(request.path, request.request_type, "")  # Creating the response
+
         server.send(response.msg)
+
+        if not response.code == "200 OK" or not response.code == "201 created":
+            pass
 
 
     """
